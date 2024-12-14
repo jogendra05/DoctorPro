941cf01ed10a4b7f4060c765344a075b56f18bc2
jogendra05 <jogendrasuthar03@gmail.com>
2024-12-14 17:40:23 +0530

    Initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home, Doctors, Login, About, MyAppointment, MyProfile, Contact, Appointment } from './pages/index.js'
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/doctors' element={<Doctors/>} />
+        <Route path='/doctors/:speciality' element={<Doctors/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/my-appointment' element={<MyAppointment/>} />
+        <Route path='/my-profile' element={<MyProfile/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/appontment/:docId' element={<Appointment/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './Home.jsx'
+import Doctors from './Doctors.jsx'
+import Appointment from './Appointment.jsx'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+import Login from './Login.jsx'
+import MyAppointment from './MyAppointment.jsx'
+import MyProfile from './MyProfile.jsx'
+
+export {
+    Home,
+    Doctors,
+    Appointment,
+    About,
+    Contact,
+    Login,
+    MyAppointment,
+    MyProfile
+}
