66f3f0935ebc698f37455eb7d2bef6ce91f6fbc3
jogendra05 <jogendrasuthar03@gmail.com>
2024-12-14 20:50:15 +0530

    Add Navbar and Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Home, Doctors, Login, About, MyAppointment, MyProfile, Contact, Appointment } from './pages/index.js'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/doctors' element={<Doctors/>} />
@@ -16,6 +19,7 @@ const App = () => {
         <Route path='/contact' element={<Contact/>} />
         <Route path='/appontment/:docId' element={<Appointment/>} />
       </Routes>
+      <Footer/>
     </div>
   )
 }
