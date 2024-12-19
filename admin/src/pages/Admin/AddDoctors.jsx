d8193b48912ebbbe973ddd2e2aa236bb10f2d2f8
jogendra05 <jogendrasuthar03@gmail.com>
2024-12-19 18:35:32 +0530

    Update AddDoctors PAge

--- admin/src/pages/Admin/AddDoctors.jsx
+++ admin/src/pages/Admin/AddDoctors.jsx
@@ -1,9 +1,8 @@
 import React, { useContext, useState } from "react";
 import { assets } from "../../assets/assets";
-import { AdminContext } from '../../contexts/AdminContext'
-import { toast } from 'react-toastify'
-import axios from 'axios'
-
+import { AdminContext } from "../../contexts/AdminContext";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const AddDoctors = () => {
   const [formData, setFormData] = useState({
@@ -16,61 +15,60 @@ const AddDoctors = () => {
     about: "",
     speciality: "General physician",
     degree: "",
-    address: {line1: "", line2: ""},
-    
+    address: { line1: "", line2: "" },
   });
 
-
-  const {aToken, backendUrl} = useContext(AdminContext)
+  const { aToken, backendUrl } = useContext(AdminContext);
 
   const onSubmitHandler = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     try {
-        
-        if (!formData.image) {
-            return toast.error('Image Not Selected')
-        }
-        
+      if (!formData.image) {
+        return toast.error("Image Not Selected");
+      }
 
-        const newForm = new FormData()
+      const newForm = new FormData();
 
-        newForm.append('image', formData.image)
-        newForm.append('name', formData.name)
-        newForm.append('email', formData.email)
-        newForm.append('password', formData.password)
-        newForm.append('experience', formData.experience)
-        newForm.append('fees', Number(formData.fees))
-        newForm.append('about', formData.about)
-        newForm.append('speciality', formData.speciality)
-        newForm.append('degree', formData.degree)
-        newForm.append('address', JSON.stringify(formData.image))
+      newForm.append("image", formData.image);
+      newForm.append("name", formData.name);
+      newForm.append("email", formData.email);
+      newForm.append("password", formData.password);
+      newForm.append("experience", formData.experience);
+      newForm.append("fees", Number(formData.fees));
+      newForm.append("about", formData.about);
+      newForm.append("speciality", formData.speciality);
+      newForm.append("degree", formData.degree);
+      newForm.append("address", JSON.stringify(formData.image));
 
-        const {data} = await axios.post(backendUrl + '/api/admin/add-doctor', newForm, {headers : {aToken}}) // here aToken will be passed and become atoken 
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/add-doctor",
+        newForm,
+        { headers: { aToken } }
+      ); // here aToken will be passed and become atoken
 
-        if (data.success) {
-            toast.success(data.message)
-            setFormData({
-                image: "",
-                name: "",
-                email: "",
-                password: "",
-                experience: "1 Year",
-                fees: "",
-                about: "",
-                speciality: "General physician",
-                degree: "",
-                address: {line1: "", line2: ""},
-            })
-        }else{
-            toast.error(data.message)
-        }
-
+      if (data.success) {
+        toast.success(data.message);
+        setFormData({
+          image: "",
+          name: "",
+          email: "",
+          password: "",
+          experience: "1 Year",
+          fees: "",
+          about: "",
+          speciality: "General physician",
+          degree: "",
+          address: { line1: "", line2: "" },
+        });
+      } else {
+        toast.error(data.message);
+      }
     } catch (error) {
-        toast.error(error.message)
-        console.log(error)
+      toast.error(error.message);
+      console.log(error);
     }
-  }
+  };
 
   return (
     <form onSubmit={onSubmitHandler} className="m-5 w-full ">
@@ -80,7 +78,11 @@ const AddDoctors = () => {
           <label htmlFor="doc-img">
             <img
               className="w-16 bg-gray-100 rounded-full cursor-pointer"
-              src={formData.image ? URL.createObjectURL(formData.image) : assets.upload_area}
+              src={
+                formData.image
+                  ? URL.createObjectURL(formData.image)
+                  : assets.upload_area
+              }
               alt=""
             />
           </label>
@@ -101,7 +103,11 @@ const AddDoctors = () => {
           <div className="w-full lg:flex-1 flex flex-col gap-4">
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Name</p>
-              <input onChange={(e) => setFormData(prev => ({...prev, name:e.target.value}))} value={formData.name}
+              <input
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, name: e.target.value }))
+                }
+                value={formData.name}
                 className="border rounded py-2 px-3"
                 type="text"
                 placeholder="Name"
@@ -111,7 +117,11 @@ const AddDoctors = () => {
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Email</p>
-              <input onChange={(e) => setFormData(prev => ({...prev, email:e.target.value}))} value={formData.email}
+              <input
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, email: e.target.value }))
+                }
+                value={formData.email}
                 className="border rounded py-2 px-3"
                 type="email"
                 placeholder="Email"
@@ -121,7 +131,11 @@ const AddDoctors = () => {
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Password</p>
-              <input onChange={(e) => setFormData(prev => ({...prev, password:e.target.value}))} value={formData.password}
+              <input
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, password: e.target.value }))
+                }
+                value={formData.password}
                 className="border rounded py-2 px-3"
                 type="password"
                 placeholder="Password"
@@ -130,8 +144,19 @@ const AddDoctors = () => {
             </div>
 
             <div className="flex-1 flex flex-col gap-1">
-                <p>Experience</p>
-              <select  onChange={(e) => setFormData(prev => ({...prev, experience:e.target.value}))} value={formData.experience} className="border rounded py-2 px-3" name="" id="">
+              <p>Experience</p>
+              <select
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    experience: e.target.value,
+                  }))
+                }
+                value={formData.experience}
+                className="border rounded py-2 px-3"
+                name=""
+                id=""
+              >
                 <option value="1">1 Year</option>
                 <option value="2">2 Years</option>
                 <option value="3">3 Years</option>
@@ -147,7 +172,11 @@ const AddDoctors = () => {
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Fees</p>
-              <input onChange={(e) => setFormData(prev => ({...prev, fees:e.target.value}))} value={formData.fees}
+              <input
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, fees: e.target.value }))
+                }
+                value={formData.fees}
                 className="border rounded py-2 px-3"
                 type="number"
                 placeholder="Fees"
@@ -159,7 +188,18 @@ const AddDoctors = () => {
           <div className="w-full lg:flex-1 flex flex-col gap-4">
             <div className="flex-1 flex flex-col gap-1">
               <p>Speciality</p>
-              <select onChange={(e) => setFormData(prev => ({...prev, speciality:e.target.value}))} value={formData.speciality} className="border rounded py-2 px-3" name="" id="">
+              <select
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    speciality: e.target.value,
+                  }))
+                }
+                value={formData.speciality}
+                className="border rounded py-2 px-3"
+                name=""
+                id=""
+              >
                 <option value="General physician">General physician</option>
                 <option value="Gynecologist">Gynecologist</option>
                 <option value="Dermatologist">Dermatologist</option>
@@ -171,7 +211,11 @@ const AddDoctors = () => {
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Education</p>
-              <input onChange={(e) => setFormData(prev => ({...prev, degree:e.target.value}))} value={formData.degree}
+              <input
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, degree: e.target.value }))
+                }
+                value={formData.degree}
                 className="border rounded py-2 px-3"
                 type="text"
                 placeholder="Education"
@@ -181,13 +225,27 @@ const AddDoctors = () => {
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Address</p>
-              <input onChange={(e) => setFormData(prev => ({...prev, address:{...prev.address , line1:e.target.value}}))} value={formData.address.line1}
+              <input
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    address: { ...prev.address, line1: e.target.value },
+                  }))
+                }
+                value={formData.address.line1}
                 className="border rounded py-2 px-3"
                 type="text"
                 placeholder="Address1"
                 required
               />
-              <input onChange={(e) => setFormData(prev => ({...prev, address:{...prev.address , line2:e.target.value}}))} value={formData.address.line2}
+              <input
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    address: { ...prev.address, line2: e.target.value },
+                  }))
+                }
+                value={formData.address.line2}
                 className="border rounded py-2 px-3"
                 type="text"
                 placeholder="Address2"
@@ -200,7 +258,11 @@ const AddDoctors = () => {
         <div>
           <div className="text-gray-600">
             <p className="mt-4 mb-2">About Doctor</p>
-            <textarea onChange={(e) => setFormData(prev => ({...prev, about:e.target.value}))} value={formData.about}
+            <textarea
+              onChange={(e) =>
+                setFormData((prev) => ({ ...prev, about: e.target.value }))
+              }
+              value={formData.about}
               className="w-full px-4 pt-2 border rounded"
               placeholder="Write about doctor"
               rows={5}
